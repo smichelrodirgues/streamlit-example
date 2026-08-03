cf644ba4f9f3287790a8526073a427ff3ec35742
Michel Rodrigues <michelmsr.rodrigues@gmail.com>
2026-08-03 13:39:44 -0300

    Inclusao da rubrica 32623 - Solicitacao PP4

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,7 @@ def processar_pdf(pdf_content):
     df['CNPJ'] = df['CNPJ'].str.replace('.', '').str.replace('/', '').str.replace('-', '')
     # inserindo rubricas que não devem sair no XML
 
-    valores_para_filtrar = ['34685', '34447', '30846','34742']
+    valores_para_filtrar = ['34685', '34447', '30846', '34742', '32623']
     ano_atual=datetime.now().year
     mes_atual = datetime.now().strftime('%m')
     # Filtrar o DataFrame
